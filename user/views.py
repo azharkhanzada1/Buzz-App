b3a4d399f332ba9b22d6b4852c8be844d55759e6
Azhar khan <azharkhanzada356@gmail.com>
2024-08-28 03:44:19 +0500

    Swagger

--- user/views.py
+++ user/views.py
@@ -1,25 +1,19 @@
-from rest_framework import generics
+from django.core.serializers import serialize
+from rest_framework import generics, status, permissions
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
 from .models import CustomUser
-# from .pagination import UserPagination
-from rest_framework.pagination import LimitOffsetPagination
-
 from .serializers import UserCreateSerializer
 from .pagination import UserLimitOffsetPagination
-# from .pagination import UserCursorPagination
-
 
 class UserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserCreateSerializer
-    # pagination_class = UserPagination
     pagination_class = UserLimitOffsetPagination
-    # pagination_class = UserCursorPagination
 
-
+@permission_classes([AllowAny])
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
 
@@ -31,36 +25,5 @@ class RegisterUserView(generics.CreateAPIView):
                 'username': user.username,
                 'email': user.email,
                 'phone_number': user.phone_number
-            }, status=status.HTTP_201_CREATED)
+                }, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#
-#
-# from rest_framework import generics, viewsets
-# from rest_framework.response import Response
-# from rest_framework import status
-# from django.contrib.auth import get_user_model
-# from .models import CustomUser
-# from .serializers import UserCreateSerializer
-# from .pagination import UserLimitOffsetPagination
-#
-# User = get_user_model()
-#
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = CustomUser.objects.all()
-#     serializer_class = UserCreateSerializer
-#     pagination_class = UserLimitOffsetPagination
-#
-# class RegisterUserView(generics.CreateAPIView):
-#     serializer_class = UserCreateSerializer
-#
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             return Response({
-#                 'username': user.username,
-#                 'email': user.email,
-#                 'phone_number': user.phone_number
-#             }, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
